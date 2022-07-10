17d0c5899cd348d192a0766a6ac7aaccc4579a26
Oleg Eterevsky <oleg@eterevsky.com>
2022-07-10 22:57:35 +0200

    Implement SYST-based timer (not precise yet since the clocks are not properly setup).

---
+++ blink-pac/README.md
@@ -0,0 +1,3 @@
+# Blink Raspberry Pico light only using PAC
+
+This example blinks the light on Raspberry Pi Pico on and off using only rp2040-pac (peripheral access crate).
--- blink-pac/src/main.rs
+++ blink-pac/src/main.rs
@@ -5,6 +5,8 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::time::Duration;
+use cortex_m::peripheral::{syst::SystClkSource, SYST};
 
 #[link_section = ".boot2"]
 #[used]
@@ -15,12 +17,48 @@ fn panic(_panic_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// External high-speed crystal on the pico board is 12Mhz
-pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
+struct Timer {
+    syst: SYST,
+}
+
+impl Timer {
+    fn new(syst: SYST) -> Self {
+        Self { syst }
+    }
+
+    fn sleep(&mut self, duration: Duration) {
+        // Microsecond timer
+        self.syst.set_clock_source(SystClkSource::External);
+
+        let duration_ticks = duration.as_micros();
+
+        let full_cycles = (duration_ticks >> 24) as u32;
+        if full_cycles > 0 {
+            self.syst.set_reload(0x00ffffff);
+            self.syst.clear_current();
+            self.syst.enable_counter();
+
+            for _ in 0..full_cycles {
+                while !self.syst.has_wrapped() {}
+            }
+        }
+
+        let ticks_left = (duration_ticks & 0x00ffffff) as u32;
+        if ticks_left > 1 {
+            self.syst.set_clock_source(SystClkSource::External);
+            self.syst.set_reload(ticks_left - 1);
+            self.syst.clear_current();
+            self.syst.enable_counter();
+
+            while !self.syst.has_wrapped() {}
+        }
+    }
+}
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let pac = rp2040_pac::Peripherals::take().unwrap();
+    let core = rp2040_pac::CorePeripherals::take().unwrap();
     let pin_id = 25;
 
     pac.RESETS.reset.modify(|_, w| w.io_bank0().set_bit());
@@ -30,7 +68,14 @@ fn main() -> ! {
     pac.IO_BANK0.gpio[pin_id].gpio_ctrl.modify(|_, w| w.funcsel().sio());
 
     pac.SIO.gpio_oe.write(|w| unsafe { w.bits(1 << pin_id) } );
-    pac.SIO.gpio_out.write(|w| unsafe { w.bits(1 << pin_id) } );
-    
-    loop {}
+
+    let mut timer = Timer::new(core.SYST);
+    let delay_500ms = Duration::from_millis(500);
+
+    loop {
+        pac.SIO.gpio_out.write(|w| unsafe { w.bits(0) } );
+        timer.sleep(delay_500ms);
+        pac.SIO.gpio_out.write(|w| unsafe { w.bits(1 << pin_id) } );
+        timer.sleep(delay_500ms);
+    }
 }
